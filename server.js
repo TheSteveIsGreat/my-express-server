67ceb5ee88181d23b07ec9942f7580a3141306f7
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-08-10 11:10:53 -0600

    routes added

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get("/", function(req, res) {
   res.send("<h1>Hello, world!</h1>")
 })
 
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: ")
+})
+
+app.get("/about", function(req, res) {
+  res.send("About me: I like coding!")
+})
+
 app.listen(3000, function(){
   console.log('Server started on port 3000')
 })
